9c850123da8e87897086412616e7738d18699c35
Tekai_ <vip007_007@mail.ru>
2023-12-11 21:52:53 +0300

    env delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/shared/ui/button";
-
-export default function Home() {
+import CoursesList from "@/features/courses-list/pub/courses-list";
+import CreateCourseForm from "@/features/courses-list/pub/create-course-form";
+export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Button</Button>
+    <main className="flex min-h-screen flex-col p-10">
+      <CreateCourseForm revalidatePagePath="/" className="mb-5 w-[300px]" />
+      <CoursesList revalidatePagePath="/" />
     </main>
   );
 }
---
+++ src/features/courses-list/actions.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { coursesRepository } from "./courses.repository";
+import { revalidatePath } from "next/cache";
+
+export const createCourseAction = async (
+  revalidatePagePath: string,
+  data: CreateCourseCommand
+) => {
+  await coursesRepository.createCourseElement(data);
+  revalidatePath(revalidatePagePath);
+};
+
+export const handleDeleteAction = async (
+  courseId: string,
+  revalidatePagePath: string
+) => {
+  "use server";
+
+  await coursesRepository.deleteCourseElement({ id: courseId });
+  revalidatePath(revalidatePagePath);
+};
