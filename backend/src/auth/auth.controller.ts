edc353c35dd5180affbfc50e7299b5446e44445b
Tekai_ <vip007_007@mail.ru>
2023-12-19 21:01:44 +0300

    Complete create user

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private userService: UserService) {}
+  @Post('register')
+  async registerUser(@Body() dto: CreateUserDto) {
+    return this.userService.createUser(dto);
+  }
+}
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PrismaService } from 'src/prisma.service';
+import { UserService } from '../user/user.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, PrismaService, UserService],
 })
 export class AuthModule {}
