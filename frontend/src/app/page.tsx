3cf59f809018e055b7e6ac9c49f42cf40e25f0c0
Tekai_ <vip007_007@mail.ru>
2024-02-13 18:29:17 +0300

    fix main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,23 +6,29 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <main className="flex min-h-full flex-col ">
-      <section className="h-screen">
-        <div className="text-9xl font-extrabold w-fit text p-10">Anchor</div>
+      <section className="h-screen pl-28">
+        <div className="text-9xl font-extrabold w-fit text pt-10 pb-10">
+          Anchor
+        </div>
 
         <LogoIcon fontSize={"100px"} className="absolute right-5 top-40" />
-        <section className="ml-9 ">
-          <div className="text-6xl font-extrabold w-fit text p-5 ">Author</div>
+        <section>
+          <div className="text-6xl font-extrabold w-fit text pb-5 pt-5">
+            Author
+          </div>
           <div className="">
-            <div className="text-4xl font-bold w-fit p-5">
+            <div className="text-4xl font-bold w-fit pt-5">
               Yakovlev Vladimir
             </div>
           </div>
-          <Link href={"/social/messages"} className=" left-[300px] top-[800px]">
-            <Button className="m-10 text-5xl w-60 h-40">Sign in</Button>
-          </Link>
-          <Link href={"/api/registration"} className="left-[300px] top-[800px]">
-            <Button className="m-10 text-5xl w-60 h-40">Sign up</Button>
-          </Link>
+          <div className="flex gap-10 mt-44 ">
+            <Link href={"/social/messages"}>
+              <Button className="p-10 text-5xl w-60 h-40">Sign in</Button>
+            </Link>
+            <Link href={"/api/registration"}>
+              <Button className="p-10 text-5xl w-60 h-40">Sign up</Button>
+            </Link>
+          </div>
         </section>
       </section>
     </main>
